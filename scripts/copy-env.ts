5f82d15e25f4f8bc28b84202c9604bebc1aca1a4
beeman <beeman@beeman.nl>
2026-02-21 18:36:04 +0800

    fix: scripts/copy-env.ts logic (#22)

--- scripts/copy-env.ts
+++ scripts/copy-env.ts
@@ -5,8 +5,11 @@ const glob = new Glob('apps/*/.env.example')
 for await (const example of glob.scan('.')) {
   const envPath = example.replace('.env.example', '.env')
   const env = file(envPath)
-  if (!(await env.exists())) {
-    await write(env, file(example))
+  const shouldCopy = !(await env.exists()) || env.size === 0
+
+  if (shouldCopy) {
+    const contents = await file(example).text()
+    await write(envPath, contents)
     console.log(`Copied ${example} → ${envPath}`)
   }
 }
